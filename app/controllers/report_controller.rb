36a8b5814baaf239a88149900ada1159fe8c7293
Jaime Giraldo <jgiraldo@rew.ca>
2021-01-26 11:11:50 -0500

    Export to xlsx

--- app/controllers/report_controller.rb
+++ app/controllers/report_controller.rb
@@ -5,5 +5,9 @@ class ReportController < ApplicationController
     @date = params[:date] && Time.zone.parse(params[:date]).utc || Time.zone.now.beginning_of_month.utc
     @end_date = @date.end_of_month
     @time_tracks = TimeTrack.between(@date..@end_date).all
+    respond_to do |format|
+      format.html
+      format.xlsx
+    end
   end
 end
